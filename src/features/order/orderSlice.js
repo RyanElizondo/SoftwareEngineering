835d02a4f07849ee0b39972a4af9430a440ae1c8
olivrz <oliver.rzep@gmail.com>
2023-03-08 20:47:06 -0500

    Begin customize order feature

---
+++ src/features/order/orderSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+    "items": {}
+}
+
+const addItemReducer = (item) => {
+
+}
+
+const options = {
+    name: 'order',
+    initialState: initialState,
+    reducers: {
+
+    }
+}
