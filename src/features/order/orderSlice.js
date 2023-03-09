577ad7c49cda75a5632dc5896f71b3bb62ca92fd
olivrz <oliver.rzep@gmail.com>
2023-03-09 17:48:54 -0500

    Create order reducers

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -1,17 +1,26 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-    "items": {}
+    "items": []
 }
 
 const addItemReducer = (item) => {
-
+    
 }
 
-const options = {
+export const orderSlice = createSlice({
     name: 'order',
     initialState: initialState,
     reducers: {
-
+        addItem: (state,action) => {
+            state.push(action.payload);
+        },
+        editItem: (state,action) => {
+            //overwrite item in customer's cart
+            state.find( item => item.id === action.payload.id).id = action.payload.id;
+        },
+        removeItem: (state, action) => {
+            state.filter(item => item.id !== action.payload.id);
+        }
     }
-}
+})
