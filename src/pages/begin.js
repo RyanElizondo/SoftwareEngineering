9c08c84ea49f1b20d99f55888488fd0d192591a2
olivrz <oliver.rzep@gmail.com>
2023-03-19 14:52:10 -0400

    Add TODO for OAuth2.0 redirect

--- src/pages/begin.js
+++ src/pages/begin.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 
-export default function Begin(props){
+export default function Begin(){
     return (
         <div className="page">
             <div className="login-links-holder">
+                {/*TODO replace below href to redirect user to Oauth2.0 API */}
                 <Link href="/menu" className="login-link">Continue with Google</Link>
                 <Link href="/menu" className="login-link">Continue as Guest</Link>
             </div>
