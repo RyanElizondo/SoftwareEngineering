b39ddb6826a90becaa74ae97889dcee406ff008c
Lucas Cordova III <lucascordovaiii12@gmail.com>
2023-03-21 10:40:35 -0400

    index and style design

--- src/pages/begin.js
+++ src/pages/begin.js
@@ -7,7 +7,10 @@ export default function Begin(){
                 <link rel="stylesheet"
                       href = "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"/>
                 {/*TODO replace below href to redirect user to Oauth2.0 API */}
+                <h2 className="logo">
+                    <img src = "google.png" alt="LogoHere" /></h2>
                 <Link href="/menu" className="login-link1">Continue with Google</Link>
+                <br></br>
                 <Link href="/menu" className="login-link2">Continue as Guest</Link>
             </div>
         </div>
