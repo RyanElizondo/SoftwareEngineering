c4dd7178502c07ce402e6f4b227e6fb979da217d
olivrz <oliver.rzep@gmail.com>
2023-03-21 15:15:33 -0400

    Create payment intent on server from client request

--- src/pages/api/create-payment-intent.js
+++ src/pages/api/create-payment-intent.js
@@ -5,7 +5,7 @@ const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 1400;
+  return ((items.reduce( (prev, curr) => prev + curr.price, 0) * 1.0625)).toFixed(2) * 100;
 };
 
 export default async function handler(req, res) {
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { useSelector } from 'react-redux'
+import { selectItems } from '../features/order/orderSlice'
 
 import CheckoutForm from "../components/CheckoutForm.jsx";
 
@@ -11,13 +13,14 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
 export default function App() {
   const [clientSecret, setClientSecret] = React.useState("");
+  const clientItems = useSelector(selectItems)
 
   React.useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     fetch("/api/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+      body: JSON.stringify({ items: clientItems }),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
