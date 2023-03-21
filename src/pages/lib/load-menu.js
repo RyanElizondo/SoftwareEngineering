c73a92c1d1c193a60b8ab4d6daae37f4f103c4bb
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-03-21 15:50:16 -0400

    directly pull from mongodb (no file writing)

--- src/pages/lib/load-menu.js
+++ src/pages/lib/load-menu.js
@@ -2,7 +2,6 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 const databaseReady = true;
-const jsonDirectory = path.join(process.cwd(), 'json'); //absolute path to json folder
 
 // The following function is shared with getStaticProps and API routes from a `lib/` directory
 export async function loadMenu() {
@@ -15,28 +14,28 @@ export async function loadMenu() {
         //connection details
         const uri = process.env.mongoURI; //uri hidden in environment variables for safety
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
-        
+        let menuItemsArray = []; ///array used to hold collection
+
         try{
             client.connect(); //attempt connection
 
             const menu = client.db('Expresso').collection('Menu'); //select menu collection
 
-            const cursor = menu.find({}); //find all documents in collection with no filter
-            let menuItemsArray =  await cursor.toArray(); //make array of documents
+            menuItemsArray =  await menu.find({}).toArray(); //fill array with documents
 
-            fs.writeFileSync(jsonDirectory + '/mongomenu.json', JSON.stringify(menuItemsArray, null, 2)); //converting array to json and saving it as file
         } catch(e){
             console.error(e);
         } finally{
             client.close(); //close connection
 
-            const fileContents = fs.readFileSync(jsonDirectory + '/mongomenu.json', 'utf8');
-
-            //Return the content of the data file in json format
-            return JSON.parse(fileContents);
+            //Return the content of collection directly in json format
+            return JSON.parse(JSON.stringify(menuItemsArray, null, 2));
         }
         
     } else {
+        //Absolute path to json folder
+        const jsonDirectory = path.join(process.cwd(), 'json'); 
+
         //Read the json data file data.json
         const fileContents = await fs.readFile(jsonDirectory + '/menudata.json', 'utf8');
 
