ece5c541bde443969a294a2ab1fa7a1c379a2040
VrajPanchal <51272809+VrajPanchal@users.noreply.github.com>
2023-03-21 13:35:58 -0400

    Add files via upload

--- src/pages/api/create-payment-intent.js
+++ src/pages/api/create-payment-intent.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "eur",
+    currency: "USD",
     automatic_payment_methods: {
       enabled: true,
     },
