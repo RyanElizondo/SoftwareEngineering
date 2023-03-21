58fd5131ac40e36b52ca27f94baa289e25a50c29
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-03-21 15:34:52 -0400

    import from mongodb async

--- src/pages/lib/load-menu.js
+++ src/pages/lib/load-menu.js
@@ -1,50 +1,42 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
-//TODO @database team: replace json data fetching with a call to MongoDB's menu collection
 const databaseReady = true;
+const jsonDirectory = path.join(process.cwd(), 'json'); //absolute path to json folder
 
 // The following function is shared with getStaticProps and API routes from a `lib/` directory
 export async function loadMenu() {
 
     if(databaseReady) {
-        const { MongoClient, ServerApiVersion } = require('mongodb'); //mongoDB package
-        const fs = require('fs'); //fs package
-        const uri = "mongodb+srv://admin:64XGZkVicmKjjzQx@cluster0.khxufgh.mongodb.net/?retryWrites=true&w=majority"; //connection url with full CRUD credentials
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); //make connection
+        //mongodb and fs packages
+        const { MongoClient, ServerApiVersion } = require('mongodb'); 
+        const fs = require('fs'); 
 
-        const jsonDirectory = path.join(process.cwd(), 'json'); //path to json folder
+        //connection details
+        const uri = process.env.mongoURI; //uri hidden in environment variables for safety
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
+        
+        try{
+            client.connect(); //attempt connection
 
-        async function run() {
-            try {
-                const database = client.db('Expresso'); //selecting db in cluster
-                const menu = database.collection('Menu'); //selecting collection in db
-            
-                const cursor = menu.find({}); //find all documents in collection with no filter
-            
-                let menuItemsList = []; 
-                await (await cursor).forEach( doc => menuItemsList.push(doc)); //storing all documents from cursor into array
+            const menu = client.db('Expresso').collection('Menu'); //select menu collection
 
-                fs.writeFileSync(jsonDirectory + '/mongomenu.json', JSON.stringify(menuItemsList, null, 2)); //converting array to json and saving it as file
-                
-            } 
-            finally {
-                await client.close(); //close connection to db
-            }
+            const cursor = menu.find({}); //find all documents in collection with no filter
+            let menuItemsArray =  await cursor.toArray(); //make array of documents
+
+            fs.writeFileSync(jsonDirectory + '/mongomenu.json', JSON.stringify(menuItemsArray, null, 2)); //converting array to json and saving it as file
+        } catch(e){
+            console.error(e);
+        } finally{
+            client.close(); //close connection
+
+            const fileContents = fs.readFileSync(jsonDirectory + '/mongomenu.json', 'utf8');
+
+            //Return the content of the data file in json format
+            return JSON.parse(fileContents);
         }
-
-        run().catch(console.dir); //catch error
-
-        //Read the json data from the file created from earlier 
-        const fileContents = await fs.readFileSync(jsonDirectory + '/mongomenu.json', 'utf8');
-
-        //Return the content of the data file in json format
-        return JSON.parse(fileContents);
         
     } else {
-        //Find the absolute path of the json directory
-        const jsonDirectory = path.join(process.cwd(), 'json');
-
         //Read the json data file data.json
         const fileContents = await fs.readFile(jsonDirectory + '/menudata.json', 'utf8');
 
