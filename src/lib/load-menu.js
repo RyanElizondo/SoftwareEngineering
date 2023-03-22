39f133af3522e5aff6528b9ab2427e7b5a1413a6
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-03-22 11:33:03 -0400

    testing async to be more robust

--- src/lib/load-menu.js
+++ src/lib/load-menu.js
@@ -1,46 +1,41 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
-const databaseReady = true;
+
 
 // The following function is shared with getStaticProps and API routes from a `lib/` directory
 export async function loadMenu() {
 
-    if(databaseReady) {
-        //mongodb and fs packages
-        const { MongoClient, ServerApiVersion } = require('mongodb'); 
-        const fs = require('fs'); 
+    //mongodb and fs packages
+    const { MongoClient, ServerApiVersion } = require('mongodb'); 
+    const fs = require('fs'); 
 
-        //connection details
-        const uri = process.env.mongoURI; //uri hidden in environment variables for safety
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
-        let menuItemsArray = []; ///array used to hold collection
+    //connection details
+    const uri = process.env.mongoURI; //uri hidden in environment variables for safety
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
+    
+    try{
+        client.connect(); //attempt connection
 
-        try{
-            client.connect(); //attempt connection
+        const menu = client.db('Expresso').collection('Menu'); //select menu collection
 
-            const menu = client.db('Expresso').collection('Menu'); //select menu collection
+        let menuItemsArray =  await menu.find({}).toArray(); //fill array with documents
 
-            menuItemsArray =  await menu.find({}).toArray(); //fill array with documents
+        client.close(); //close connection
 
-        } catch(e){
-            console.error(e);
-        } finally{
-            client.close(); //close connection
+        //Return the content of collection directly in json format
+        return JSON.parse(JSON.stringify(menuItemsArray, null, 2));
 
-            //Return the content of collection directly in json format
-            return JSON.parse(JSON.stringify(menuItemsArray, null, 2));
-        }
-        
-    } else {
+    } catch(e){
+        console.error(e); //output error
+
         //Absolute path to json folder
         const jsonDirectory = path.join(process.cwd(), 'json'); 
 
         //Read the json data file data.json
-        const fileContents = await fs.readFile(jsonDirectory + '/menudata.json', 'utf8');
+        const fileContents = fs.readFileSync(jsonDirectory + '/menudata.json', 'utf8');
 
         //Return the content of the data file in json format
         return JSON.parse(fileContents);
     }
-
 }
