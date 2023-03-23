d9aa3b5584f305e6a58a7ddc41bccbae0b086893
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-03-23 16:07:55 -0400

    starting CRUD operations

--- mongoCRUD.js
+++ mongoCRUD.js
@@ -1,14 +1,15 @@
 const { MongoClient, ServerApiVersion } = require('mongodb'); //mongodb package
-const uri = process.env.mongoURI; //uri hidden in environment variables for safety
 
-var _db;              
+var client;
+var _db;
 
+//opens mongo connection and saves _db global variable to be used throughout code
 export async function openMongoConnection() {
 
     try{
-               
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); //connection details
-
+        let uri = process.env.mongoURI; //uri hidden in environment variables for safety  
+        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); //connection details
+        
         client.connect(); //open connection
 
         _db = client.db('Expresso'); //select db collection
@@ -18,6 +19,7 @@ export async function openMongoConnection() {
     }
 }
 
+//closes mongo connection though client global var
 export async function closeMongoConnection(){
     try{
         client.close(); //close connection
@@ -26,16 +28,18 @@ export async function closeMongoConnection(){
     }
 }
 
+//creates 1 user given a json object
 export async function createUser(userJsonObject){
     try{
         let insertedUser = _db.collection('Users').insertOne(userJsonObject); //insert one given a json object
-        console.log(`Successfully created user with _id: ${insertedUser.insertedId}`); 
+        console.log(`Successfully created user with id: ${insertedUser.insertedId}`); 
 
     } catch(e){
         console.log("ERROR: Could not create user");
     }
 }
 
+//creates 1 menu item given a json object
 export async function createMenuItem(menuJsonObject){
     try{
         let insertedMenu = _db.collection('Menu').insertOne(menuJsonObject); //insert one given a json object
@@ -46,6 +50,7 @@ export async function createMenuItem(menuJsonObject){
     }
 }
 
+//creates 1 order given a json object
 export async function createOrder(orderJsonObject){
     try{
         let insertedOrder = _db.collection('Orders').insertOne(orderJsonObject); //insert one given a json object
@@ -56,28 +61,65 @@ export async function createOrder(orderJsonObject){
     }
 }
 
-export async function readUser(){
+//looks for 1 user that matches filters that are given as a json object
+export async function readUser(queries){
+    try{
+        let findUser = _db.collection('Users').findOne(queries); 
+        console.log(`Found user: ${findUser}`); 
 
+    } catch(e){
+        console.log("ERROR: Could not find user");
+    }
 }
 
-export async function readMenuItem(){
+export async function readMenuItem(queries){
+    try{
+        let findMenuItem = _db.collection('Menu').findOne(queries); 
+        console.log(`Found menu item: ${findMenuItem}`); 
 
+    } catch(e){
+        console.log("ERROR: Could not find menu item");
+    }
 }
 
-export async function readOrder(){
+export async function readOrder(queries){
+    try{
+        let findOrder = _db.collection('Orders').findOne(queries); 
+        console.log(`Found order: ${findOrder}`); 
 
+    } catch(e){
+        console.log("ERROR: Could not find order");
+    }
 }
 
-export async function updateUser(){
+export async function updateUser(mongoID, updatesToBeMade){
+    try{
+        _db.collection('Users').updateOne(mongoID, updatesToBeMade); 
+        console.log(`Updated user: ${_db.collection('Users').findOne(mongoID)}`); 
 
+    } catch(e){
+        console.log("ERROR: Could not update user, check if they exist first");
+    }
 }
 
-export async function updateMenuItem(){
+export async function updateMenuItem(mongoID, updatesToBeMade){
+    try{
+        _db.collection('Menu').updateOne(mongoID, updatesToBeMade); 
+        console.log(`Updated menu item: ${_db.collection('Menu').findOne(mongoID)}`); 
 
+    } catch(e){
+        console.log("ERROR: Could not update menu item, check if it exist first");
+    }
 }
 
-export async function updateOrder(){
+export async function updateOrder(mongoID, updatesToBeMade){
+    try{
+        _db.collection('Orders').updateOne(mongoID, updatesToBeMade); 
+        console.log(`Updated order: ${_db.collection('Orders').findOne(mongoID)}`); 
 
+    } catch(e){
+        console.log("ERROR: Could not update order, check if it exist first");
+    }
 }
 
 export async function deleteUser(){
@@ -102,14 +144,18 @@ export async function deleteOrder(){
 
 export async function getMenuFromMongo() {  
     try{
-        const menu = _db.collection('Menu'); //select menu collection
+
+        let menu = _db.collection('Menu'); //select menu collection
 
         let menuItemsArray =  await menu.find({}).toArray(); //fill array with documents
 
-        return JSON.stringify(menuItemsArray, null, 2); //Return the content of collection directly in json format
+        var jsonMenu = await JSON.stringify(menuItemsArray, null, 2); //Return the content of collection directly in json format
+
+        //console.log(typeof(jsonMenu));
+        return jsonMenu;
 
     } catch(e){
-        console.error(e); //output error
+        //console.error(e); //output error
     }
 }
 
