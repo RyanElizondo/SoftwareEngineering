dddebd3eb7e16e46ab463b9813e377d1ee5d1597
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-03-23 17:15:04 -0400

    load-menu final revision

--- src/lib/load-menu.js
+++ src/lib/load-menu.js
@@ -1,29 +1,28 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+import { openMongoConnection, getMenuFromMongo, closeMongoConnection } from 'mongoCRUD';
 
 // The following function is shared with getStaticProps and API routes from a `lib/` directory
 export async function loadMenu() {
 
-    const { MongoClient, ServerApiVersion } = require('mongodb'); //mongodb package
-    
-    //connection details
-    const uri = process.env.mongoURI; //uri hidden in environment variables for safety
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
-    
     try{
-        client.connect(); //attempt connection
+        openMongoConnection();
 
-        const menu = client.db('Expresso').collection('Menu'); //select menu collection
+        var menu = await getMenuFromMongo();
+        
+        /* uncomment to write to json folder and see what getMenuFromMongo() returns
+        const jsonDirectory = path.join(process.cwd(), 'json');  //Absolute path to json folder
+        await fs.writeFile (jsonDirectory + '/mongomenu.json', menu) //writing to json data file
+        const fileContents = await fs.readFile(jsonDirectory + '/mongomenu.json', 'utf8'); //Read the json data file
+        */
+        closeMongoConnection();
 
-        let menuItemsArray =  await menu.find({}).toArray(); //fill array with documents
-
-        client.close(); //close connection
-
-        //Return the content of collection directly in json format
-        return JSON.parse(JSON.stringify(menuItemsArray, null, 2));
-
+        //Return the content of the data file in json format
+        return JSON.parse(menu);
+        
     } catch(e){
-               
+        console.error(e); 
+
         //Absolute path to json folder
         const jsonDirectory = path.join(process.cwd(), 'json'); 
 
