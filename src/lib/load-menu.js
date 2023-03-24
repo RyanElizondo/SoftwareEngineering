06e75a0cf34e2e8627a6cdf73432c3410e73235d
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-03-24 13:33:31 -0400

    fixing return values

--- src/lib/load-menu.js
+++ src/lib/load-menu.js
@@ -1,12 +1,35 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 import { openMongoConnection, getMenuFromMongo, closeMongoConnection, deleteUser, createUser, updateUser } from 'mongoCRUD';
+import { setTimeout } from 'timers/promises';
 
 // The following function is shared with getStaticProps and API routes from a `lib/` directory
 export async function loadMenu() {
 
     try{
         openMongoConnection();
+
+        let newID = await createUser({name:"hmmm", OAUTHID:"randomString"});
+
+        //console.log(newID);
+        //console.log("waiting 5 secs");
+        //await setTimeout(5000);
+        
+
+       // updateUser(newID, {points: "5000", email: "testing@gmail.com"});
+
+        //console.log("waiting 5 secs");
+        //await setTimeout(5000);
+
+        console.log("DONE!")
+        //deleteUser(newID);
+
+
+
+
+
+
+
 
         var menu = await getMenuFromMongo();
         
@@ -15,6 +38,7 @@ export async function loadMenu() {
         await fs.writeFile (jsonDirectory + '/mongomenu.json', menu) //writing to json data file
         const fileContents = await fs.readFile(jsonDirectory + '/mongomenu.json', 'utf8'); //Read the json data file
         */
+
         closeMongoConnection();
 
         //Return the content of the data file in json format
