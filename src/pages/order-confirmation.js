478520863a4568e6b6998b42f97a27ceeaef14c6
ayetida21 <aditya.kesari210@gmail.com>
2023-04-12 17:11:00 -0400

    fixed orders.js

--- src/pages/order-confirmation.js
+++ src/pages/order-confirmation.js
@@ -1,6 +1,7 @@
 import {loadUserOrder} from "@/lib/load-user-order";
 import UserOrderItemsList from "@/components/UserOrderItemsList";
 import Link from "next/link";
+import Head from "next/head";
 
 /**
  * Landing page after user successfully places an order.
@@ -11,20 +12,33 @@ import Link from "next/link";
 export default function orderConfirmation( {userOrder} ) {
 
     return (
-        <div className="page">
-            <div className="return-home-button-holder">
-                <Link href='/' className="return-home-button grow">Expresso Cafe</Link>
+        <>
+            <Head>
+
+                <title>Order Confirmation</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet"
+                      href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@700&display=swap"/>
+            </Head>
+
+        <div className="page confirmation-picture">
+            <div className="return-home-button-holder grow">
+                <Link href='/' className="return-home-button">Back to Home</Link>
             </div>
+
             <h1 className="confirmation-detail name">{`Thank you for your order, ${userOrder.firstName}!`}</h1>
             <div className="confirmation-holder">
-                <h3 className="confirmation-detail orderID">{`Order number: ${userOrder.orderID}`}</h3>
-                <h5 className="confirmation-detail date">{`Date placed: ${userOrder.localeDate}`}</h5>
-                <h5 className="confirmation-detail status">{`Order status: ${userOrder.status}`}</h5>
+                {"Your Order:"}
                 <UserOrderItemsList
                     orderItems={userOrder.items}
                 />
+                <h5 className="confirmation-detail date">{`Date & Time Placed: ${userOrder.localeDate}`}</h5>
+                <h5 className="confirmation-detail price">{`Order Total: ${userOrder.TotalCost}`}</h5>
+                <h5 className="confirmation-detail status">{`Order Status: ${userOrder.status}`} </h5>
             </div>
         </div>
+        </>
     )
 }
 
