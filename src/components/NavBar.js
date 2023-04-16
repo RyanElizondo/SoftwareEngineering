5670ce11e9e9221c935d61f123d20776589cd992
Lucas Cordova <lucascordovaiii12@gmail.com>
2023-04-15 21:07:33 -0400

    design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,21 +8,24 @@ import Link from 'next/link';
 export default function NavBar() {
     return (
         <div className="navbar">
-            <h2 className="nav-button">
+            <h2 className="logo">
+                <img src = "google.png" alt="LogoHere" /></h2>
+            <h2 className="nav-button1">
                 <Link href="/">Home</Link>
             </h2>
-            <h2 className="nav-button">
+            <h2 className="nav-button2">
                 <Link href="menu">Menu</Link>
             </h2>
-            <h2 className="nav-button">
+            <h2 className="nav-button3">
                 <Link href="about-us">About us</Link>
             </h2>
-            <h2 className="nav-button">
+            <h2 className="nav-button4">
                 <Link href="contact">Contact</Link>
             </h2>
-            <h2 className="nav-button">
+            <h2 className="nav-button5">
                 <Link href="profile">Profile</Link>
             </h2>
+
         </div>
     )
 }
