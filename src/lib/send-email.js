d722d3b584c7bd65436a107a967aefdc599f78d5
Vraj Panchal <vrajpanchal3@gmail.com>
2023-04-19 17:41:14 -0400

    Added email functions

---
+++ src/lib/send-email.js
@@ -0,0 +1,8 @@
+export const sendContactForm = async (data) => fetch('/api/contact',{
+        method: "POST",
+        body: JSON.stringify(data),
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+})
