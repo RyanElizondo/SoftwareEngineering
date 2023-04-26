597003528a3d91352e8c6db611768b9265390f95
olivrz <oliver.rzep@gmail.com>
2023-04-25 23:48:49 -0400

    Get next-auth working for both customer and staff

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -100,16 +100,95 @@ const staffAuthOptions = {
     }
 };
 
+const allAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            profile(profile) {
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: 'customer'
+                }
+            }
+        }),
+        CredentialsProvider({
+            id: 'manager-login',
+            name: 'Manager Login',
+            credentials: {
+                username: { label: 'Username', type: 'text' },
+                password: { label: 'Password', type: 'password' }
+            },
+            async authorize(credentials) {
+                const user = { id: '1', name: 'Manager' };
+                if (credentials.username === process.env.MANAGER_USERNAME &&
+                    credentials.password === process.env.MANAGER_PASSWORD)
+                    return user;
+                else
+                    return null;
+            }
+        }),
+        CredentialsProvider({
+            id: 'foodprep-login',
+            name: 'FoodPrep Login',
+            credentials: {
+                username: { label: 'Username', type: 'text' },
+                password: { label: 'Password', type: 'password' }
+            },
+            async authorize(credentials) {
+                const user = { id: '2', name: 'FoodPrep' };
+                if (credentials.username === process.env.FOODPREP_USERNAME &&
+                    credentials.password === process.env.FOODPREP_PASSWORD)
+                    return user;
+                else
+                    return null;
+            }
+        })
+    ],
+    callbacks: {
+        async session({session, token}) {
+            session.user.userID = token.sub;
+            session.user.role = token.role;
+            return session;
+        },
+        async signIn({ user }) {
+            await fetch("http://localhost:9999/.netlify/functions/customer", {
+                method: "PUT",
+                body: JSON.stringify(user)
+                })
+            return true
+
+        },
+            async jwt({token, user}) {
+                if(user) token.role = user.role;
+                return token;
+            }
+        }
+    };
+
 export default (req, res) => {
-    const referringUrl = req.headers.referer;
-    const referringUrlObj = url.parse(referringUrl);
-    const referringPath = referringUrlObj.pathname;
-    console.log("PATH!");
-    console.log(referringPath);
-    if(referringPath === '/staff/login') {
-        NextAuth(req, res, staffAuthOptions);
-    } else {
-        NextAuth(req, res, customerAuthOptions);
+    try {
+        const referringUrl = req.headers.referer;
+        console.log("REFERRER");
+        console.log(referringUrl);
+        const referringUrlObj = url.parse(referringUrl);
+        const referringPath = referringUrlObj.pathname;
+        console.log("PATH!");
+        console.log(referringPath);
+        if(referringPath === '/staff/login') {
+            NextAuth(req, res, staffAuthOptions);
+        } else if(referringPath === '/begin') {
+            console.log('calling customerAuth');
+            NextAuth(req, res, customerAuthOptions);
+        } else {
+            NextAuth(req, res, allAuthOptions);
+        }
+    } catch(e) {
+        console.log("RENDERING ALL AUTH OPTIONS");
+        NextAuth(req, res, allAuthOptions);
     }
 
 };
